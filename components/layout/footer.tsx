698f007a425af23d90ebbdd5ca55db1873e07082
ReGeLePuMa <scpetrea@gmail.com>
2023-11-18 15:06:57 +0200

    Changed Footer + Favicon

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,4 +1,4 @@
-import { BuyMeACoffee } from "../shared/icons";
+import Image from "next/image";
 
 export default function Footer() {
   return (
@@ -7,21 +7,21 @@ export default function Footer() {
         A project by{" "}
         <a
           className="font-semibold text-gray-600 underline-offset-4 transition-colors hover:underline"
-          href="https://twitter.com/steventey"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Steven Tey
         </a>
       </p>
-      <a
-        href="https://www.buymeacoffee.com/steventey"
+      <a className="mx-auto mt-2 flex max-w-fit items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-sky-950 px-6 py-2 transition-all duration-75 hover:scale-105"
+        href="https://github.com/ReGeLePuMa/CyberCrab"
         target="_blank"
         rel="noopener noreferrer"
-        className="mx-auto mt-2 flex max-w-fit items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-6 py-2 transition-all duration-75 hover:scale-105"
-      >
-        <BuyMeACoffee className="h-6 w-6" />
-        <p className="font-medium text-gray-600">Buy me a coffee</p>
+        >
+      <Image
+              src="/icon.png"
+              alt="Precedent logo"
+              width= {200}
+              height = {150}
+              className="mr-2 rounded-sm"
+            ></Image> 
       </a>
     </div>
   );
