97017334b0b0a9082cc753bfcae33e91d62971f3
ReGeLePuMa <scpetrea@gmail.com>
2023-11-18 20:37:40 +0200

    Added Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { Github, Twitter } from "@/components/shared/icons";
 import WebVitals from "@/components/home/web-vitals";
 import ComponentGrid from "@/components/home/component-grid";
 import Image from "next/image";
-import { nFormatter } from "@/lib/utils";
 
 export default async function Home() {
   const { stargazers_count: stars } = await fetch(
@@ -22,60 +21,48 @@ export default async function Home() {
   )
     .then((res) => res.json())
     .catch((e) => console.log(e));
-
   return (
     <>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-        <h5
-          className="animate-fade-up bg-[conic-gradient(var(--tw-gradient-stops))] from-sky-950  to-black bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
-          style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
-        >
-          CyberCrab
-        </h5>
-        <p
-          className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
-          style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-        </p>
+        <div className="mt-4 flex flex-col flex-grow items-center justify-center p-4">
+          <video
+            className="absolute left-0 top-0 h-screen w-full object-cover"
+            autoPlay
+            loop
+            muted
+            playsInline
+          >
+            <source src="video.mp4" type="video/mp4" />
+          </video>
+          <h5
+            className="text-{128px} animate-fade-up bg-[conic-gradient(var(--tw-gradient-stops))] from-sky-950  to-white bg-clip-text text-center font-display font-extrabold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
+            style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
+          >
+            CyberCrab
+          </h5>
+          <p
+            className="mt-6 block animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
+            style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          </p>
         <div
           className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
           style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
         >
-          <a
-            className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
-            href={DEPLOY_URL}
-            target="_blank"
+          <button
+            className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-sky-950 px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-sky-950"
             rel="noopener noreferrer"
           >
-            <svg
-              className="h-4 w-4 group-hover:text-black"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 4L20 20H4L12 4Z"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <p>Deploy to Vercel</p>
-          </a>
-          <a
-            className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
-            href="https://github.com/steven-tey/precedent"
-            target="_blank"
+            <p>Login</p>
+          </button>
+          <button
+            className="flex max-w-fit max-h-fit items-center justify-center space-x-2 rounded-full border border-black bg-white px-5 py-2 text-sm text-black shadow-md transition-colors hover:text-white hover:bg-sky-950"
             rel="noopener noreferrer"
           >
-            <Github />
-            <p>
-              <span className="hidden sm:inline-block">Star on</span> GitHub{" "}
-              <span className="font-semibold">{nFormatter(stars)}</span>
-            </p>
-          </a>
+            <p>Register</p>
+            </button>
+            </div>
         </div>
       </div>
       <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
@@ -155,3 +142,5 @@ const features = [
     ),
   },
 ];
+
+
